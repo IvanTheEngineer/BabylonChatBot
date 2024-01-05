b9f21887b29d58bdef99af012d023f9ceda00403
Ivan Kisselev <ivankisselev7@gmail.com>
2024-01-05 15:05:53 -0500

    prototyping with api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import * as React from 'react';
 import './App.css'
 import TextField from '@mui/material/TextField';
@@ -6,14 +6,61 @@ import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
+import { OpenAI } from "openai";
 
 function App() {
+
+  // creates api instance
+  const openai = new OpenAI({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true
+  })
+
+  // variable to store thread id
+  const [threadID, setThreadID] = useState("");
+
+  // function to make a thread and set the id
+  const createThread = async () => {
+    const thread = await openai.beta.threads.create();
+    setThreadID(thread.id)
+  }
+
+  // one time function to create a thread at the beginning
+  useEffect(() => {
+  createThread()
+  }, []);
+
+  //prints thread id (debugging)
+  console.log(threadID)
+
+  // stores message typed by user
   const [message, setMessage] = useState("")
-  
-  const handleSubmit = () => {
+
+  // function to add messages to the thread
+  const addMessage = async () => {
+    const newmessage = await openai.beta.threads.messages.create(
+      threadID,
+      {
+        role: "user",
+        content: message
+      }
+    );
+  }
+
+  // variable to store/update chat log
+  const [chatLog, setChatLog] = useState({});
+
+  // function to get messages
+  const getMessages = async () => {
+  }
+
+  // takes care of everything that needs to be done when user sends message
+  const handleSubmit = async () => {
     console.log(message);
+    addMessage();
     setMessage("");
   };
+
   return (
     <>
       <h1>Babylon Chat Bot</h1>
