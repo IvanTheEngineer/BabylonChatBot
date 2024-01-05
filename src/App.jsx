c9a0560ed136d4b2b8b902deb098285bf8f39e11
mirelahavran <mhl6x@virginia.edu>
2024-01-05 11:19:48 -0500

    more styling

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,12 @@ function App() {
   return (
     <>
       <h1>Babylon Chat Bot</h1>
-      <TextField id="outlined-basic" label="Message" variant="outlined" value={message} 
+      <TextField id="outlined-basic" label="Type a message" variant="outlined" value={message} 
       onChange={(e) => {
         setMessage(e.target.value);
-      }}
+      }} className="myMessageStyle"
       />
       <Button onClick={() => handleSubmit()} variant="contained" endIcon={<SendIcon />} className="myButtonStyle">
-      Send
       </Button>
 
     </>
