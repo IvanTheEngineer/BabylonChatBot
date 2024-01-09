1370d8d74ed1c9d574dde8a30c573c6e5755423d
Ivan Kisselev <ivankisselev7@gmail.com>
2024-01-09 11:30:48 -0500

    loading bar

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import "./ChatBox.css"
 import { Message } from "./Message.jsx"
+import CircularProgress from '@mui/material-next/CircularProgress';
 
 export const Chat = ({messageLog, userLog}) => {
+
+    const waiting = (userLog[userLog.length-1] === "user");
 
     if (messageLog) {
         return (
     <div className="chatBox">
-        <ul>    
             {messageLog.map((element, index) => (
             <Message message={element} sender={userLog[index]}/>
         ))}
-        </ul>
+        {waiting && <CircularProgress color="info" style={{marginLeft:"5px"}}/>}
     </div>)
     }
 }
